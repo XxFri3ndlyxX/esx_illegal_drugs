d8bc1346f1341fe69b2cc2536109e22672e71b91
XxFri3ndlyxX <18711345+XxFri3ndlyxX@users.noreply.github.com>
2019-02-20 13:41:27 -0500

    Update esx_Illegal_drugs.sql

--- esx_Illegal_drugs.sql
+++ esx_Illegal_drugs.sql
@@ -1,10 +1,10 @@
 INSERT INTO `items` (name, label) VALUES
-	('weed', 'Weed'),
-	('weed_pooch', 'Pochon de weed'),
-	('coke', 'Coke'),
-	('coke_pooch', 'Pochon de coke'),
-	('meth', 'Meth'),
-	('meth_pooch', 'Pochon de meth'),
-	('opium', 'Opium'),
-	('opium_pooch', 'Pochon de opium')
+	('weed', 'Weed (1G)'),
+	('weed_pooch', 'Bag of weed (28G)'),
+	('coke', 'Coke (1G)'),
+	('coke_pooch', 'Bag of coke (28G)'),
+	('meth', 'Meth (1G)'),
+	('meth_pooch', 'Bag of meth (28G)'),
+	('opium', 'Opium (1G)'),
+	('opium_pooch', 'Bag of opium (28G)')
 ;
