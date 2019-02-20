55757e52f0cbae35c8514b59e704b42b354a63f6
XxFri3ndlyxX <18711345+XxFri3ndlyxX@users.noreply.github.com>
2019-02-20 13:25:46 -0500

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,40 @@
+Config              = {}
+Config.MarkerType   = -1
+Config.DrawDistance = 100.0
+Config.ZoneSize     = {x = 3.0, y = 3.0, z = 2.0}
+Config.MarkerColor  = {r = 255, g = 0, b = 0}
+Config.RequiredCopsCoke = 0
+Config.RequiredCopsMeth = 0
+Config.RequiredCopsWeed = 0
+Config.RequiredCopsOpium = 0
+Config.Locale = 'en'
+
+Config.Zones = {
+	CokeFarm = 		 {x=1093.139,  y=-3195.673, z=-39.131},
+	CokeTreatment =  {x=1101.837, y=-3193.732, z=-38.993},
+	CokeResell = 	 {x=959.117, y=-121.055,  z=74.963},
+	MethFarm = 		 {x=1005.721,   y=-3200.301,  z=-38.519},
+	MethTreatment =  {x=1014.878, y=-3194.871,  z=-38.993},
+	MethResell = 	 {x=7.981, y=6469.067, z=31.528},
+	WeedFarm = 		 {x=1057.448,  y=-3197.646,  z=-39.138},
+	WeedTreatment =  {x=1037.527,  y=-3205.368,  z=-38.17},
+	WeedResell = 	 {x = 85.58, y = -1959.34, z = 20.13},
+	OpiumFarm = 		 {x=2433.804,  y=4969.196, z=42.348},
+	OpiumTreatment =   {x=2434.43, y=4964.18, z=42.348},
+	OpiumResell = 	 {x=-3155.608, y=1125.368,  z=20.858}
+}
+
+Config.Map = {
+ --{name="Coke Farm",       color=6, scale=0.8, id=403, x = 47.842, y = 3701.961, z = 40.722},
+  --{name="Coke Treatment",  color=6, scale=0.8, id=403, x=-458.13967895508, y=-2278.6174316406, z=7.5158290863037},
+  --{name="Coke Sales",      color=8, scale=0.8, id=384, x=959.117, y=-121.055,  z=74.963},
+  --{name="Meth Farm",       color=6, scale=0.8, id=403, x = 1386.659, y = 3622.805, z = 35.012},
+  --{name="Meth Treatment",  color=6, scale=0.8, id=403, x=-1001.4151611328, y=4848.0087890625,  z=274.00686645508},
+  --{name="Meth Sales",      color=3, scale=0.8, id=383, x=7.981, y=6469.067, z=31.528},
+  --{name="Opium Farm",      color=6, scale=0.8, id=403, x=1972.784790039,   y=3819.3999023438,  z=33.428722381592},
+  --{name="Opium Treatment", color=6, scale=0.8, id=403, x=971.86499023438,  y=-2157.61328125,   z=28.475107192994},
+  --{name="Opium Sales",     color=6, scale=0.8, id=403, x=-3155.608, y=1125.368,  z=16.884071350098},
+  --{name="Weed Farm",       color=2, scale=0.8, id=140, x=2221.858,  y=5614.81,  z=54.902},
+  --{name="Weed Treatment",  color=2, scale=0.8, id=140, x=2467.8371582031,  y=3784.6157226563,  z=40.981040954591},
+  --{name="Weed Sales",      color=2, scale=0.8, id=140, x = 85.58, y = -1959.34, z = 20.13}
+}
---
+++ esx_drugs.sql
@@ -0,0 +1,10 @@
+INSERT INTO `items` (name, label) VALUES
+	('weed', 'Weed'),
+	('weed_pooch', 'Pochon de weed'),
+	('coke', 'Coke'),
+	('coke_pooch', 'Pochon de coke'),
+	('meth', 'Meth'),
+	('meth_pooch', 'Pochon de meth'),
+	('opium', 'Opium'),
+	('opium_pooch', 'Pochon de opium')
+;
