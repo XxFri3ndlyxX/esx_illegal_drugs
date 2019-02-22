9b63a34cfa4fab82316b9dae2b3f299874c29acb
XxFri3ndlyxX <18711345+XxFri3ndlyxX@users.noreply.github.com>
2019-02-22 01:59:47 -0500

    Update __resource.lua

--- __resource.lua
+++ __resource.lua
@@ -1,21 +1,19 @@
-description 'esx_illegal_drugs'
+resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
 
-version '1.0.0'
+description 'ESX Drugs'
+
+version '1.0.2'
 
 server_scripts {
-
-  '@es_extended/locale.lua',
+	'@es_extended/locale.lua',
 	'locales/en.lua',
-  'server/esx_illegal_drugs_sv.lua',
-  'config.lua'
-
+	'server/esx_illegal_drugs_sv.lua',
+	'config.lua'
 }
 
 client_scripts {
-
-  '@es_extended/locale.lua',
+	'@es_extended/locale.lua',
 	'locales/en.lua',
-  'config.lua',
-  'client/esx_illegal_drugs_cl.lua'
-
+	'config.lua',
+	'client/esx_illegal_drugs_cl.lua'
 }
