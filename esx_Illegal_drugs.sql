360b0c29d131abbd6c8c7565e03c9fb8c88316d0
XxFri3ndlyxX <18711345+XxFri3ndlyxX@users.noreply.github.com>
2019-02-22 01:57:37 -0500

    Added item limit to the drug

--- esx_Illegal_drugs.sql
+++ esx_Illegal_drugs.sql
@@ -1,12 +1,12 @@
 USE `essentialmode`;
 
-INSERT INTO `items` (name, label) VALUES
-	('weed', 'Weed (1G)'),
-	('weed_pooch', 'Bag of weed (28G)'),
-	('coke', 'Coke (1G)'),
-	('coke_pooch', 'Bag of coke (28G)'),
-	('meth', 'Meth (1G)'),
-	('meth_pooch', 'Bag of meth (28G)'),
-	('opium', 'Opium (1G)'),
-	('opium_pooch', 'Bag of opium (28G)')
+INSERT INTO `items` (name, label, `limit`) VALUES
+	('weed', 'Weed (1G)', 420),
+	('weed_pooch', 'Bag of weed (28G)', 15),
+	('coke', 'Coke (1G)', 420),
+	('coke_pooch', 'Bag of coke (28G)', 15),
+	('meth', 'Meth (1G)', 420),
+	('meth_pooch', 'Bag of meth (28G)', 15),
+	('opium', 'Opium (1G)', 420),
+	('opium_pooch', 'Bag of opium (28G)', 15)
 ;
