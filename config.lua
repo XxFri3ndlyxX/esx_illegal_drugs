e0fdd85784173c1d66a9a22390a6f731f42a8dfd
XxFri3ndlyxX <18711345+XxFri3ndlyxX@users.noreply.github.com>
2019-04-01 08:08:21 -0400

    Added description to help new users

--- config.lua
+++ config.lua
@@ -1,29 +1,29 @@
 Config              = {}
-Config.MarkerType   = -1
-Config.DrawDistance = 100.0
-Config.ZoneSize     = {x = 5.0, y = 5.0, z = 3.0}
-Config.MarkerColor  = {r = 0, g = 255, b = 0}
+Config.MarkerType   = -1 -- Marker visible or not. -1 = hiden 1 is one of the many marker. https://docs.fivem.net/game-references/markers/
+Config.DrawDistance = 100.0 --Distance where the marker be visible from
+Config.ZoneSize     = {x = 5.0, y = 5.0, z = 3.0} -- Size of the marker
+Config.MarkerColor  = {r = 0, g = 255, b = 0} --Color of the marker
 
-Config.RequiredCopsCoke  = 0
-Config.RequiredCopsMeth  = 0
-Config.RequiredCopsWeed  = 0
-Config.RequiredCopsOpium = 0
+Config.RequiredCopsCoke  = 0 --Ammount of cop that need to be online to be able to harvest/process/sell coke
+Config.RequiredCopsMeth  = 0 --Ammount of cop that need to be online to be able to harvest/process/sell meth
+Config.RequiredCopsWeed  = 0 --Ammount of cop that need to be online to be able to harvest/process/sell weed
+Config.RequiredCopsOpium = 0 --Ammount of cop that need to be online to be able to harvest/process/sell opium
 
-Config.TimeToFarmWeed     = 2  * 1000
-Config.TimeToProcessWeed  = 4  * 1000
-Config.TimeToSellWeed     = 1  * 1000
+Config.TimeToFarmWeed     = 2  * 1000 -- Ammount of time to harvest weed
+Config.TimeToProcessWeed  = 4  * 1000 -- Ammount of time to process weed
+Config.TimeToSellWeed     = 1  * 1000 -- Ammount of time to sell weed
 
-Config.TimeToFarmOpium    = 4  * 1000
-Config.TimeToProcessOpium = 6  * 1000
-Config.TimeToSellOpium    = 1  * 1000
+Config.TimeToFarmOpium    = 4  * 1000 -- Ammount of time to harvest coke
+Config.TimeToProcessOpium = 6  * 1000 -- Ammount of time to process coke
+Config.TimeToSellOpium    = 1  * 1000 -- Ammount of time to sell coke
 
-Config.TimeToFarmCoke     = 6  * 1000
-Config.TimeToProcessCoke  = 8  * 1000
-Config.TimeToSellCoke     = 1  * 1000
+Config.TimeToFarmCoke     = 6  * 1000 -- Ammount of time to harvest coke
+Config.TimeToProcessCoke  = 8  * 1000 -- Ammount of time to process coke
+Config.TimeToSellCoke     = 1  * 1000 -- Ammount of time to sell coke
 
-Config.TimeToFarmMeth     = 8  * 1000
-Config.TimeToProcessMeth  = 10 * 1000
-Config.TimeToSellMeth     = 1  * 1000
+Config.TimeToFarmMeth     = 8  * 1000 -- Ammount of time to harvest meth
+Config.TimeToProcessMeth  = 10 * 1000 -- Ammount of time to process meth
+Config.TimeToSellMeth     = 1  * 1000 -- Ammount of time to sell meth
 
 Config.Locale = 'en'
 
@@ -42,7 +42,7 @@ Config.Zones = {
 	OpiumDealer =		{x=-3155.608, y=1125.368,   z=20.858}
 }
 
-Config.DisableBlip = true
+Config.DisableBlip = true -- Set to true to disable blips. False to enable them.
 Config.Map = {
 
   {name="Coke Farm Entrance",    color=4, scale=0.8, id=501, x=47.842,     y=3701.961,   z=40.722},
