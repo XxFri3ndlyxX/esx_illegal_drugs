5581c9286d54546d39b5c96e5986f028533c1542
Elias H <E.Hackradt@web.de>
2020-04-26 12:04:53 +0200

    reverted translation back to englisch

--- esx_Illegal_drugs.sql
+++ esx_Illegal_drugs.sql
@@ -2,11 +2,11 @@ USE `es_extended`;
 
 INSERT INTO `items` (`name`, `label`, `weight`, `rare`, `can_remove`) VALUES
 	('weed', 'Weed (1G)', 1, 0, 1),
-	('weed_pooch', 'Weed Tasche (28G)', 28, 0, 1),
-	('coke', 'Cocain (1G)', 1, 0, 1),
-	('coke_pooch', 'Cocain Tasche (28G)', 28, 0, 1),
+	('weed_pooch', 'Bag of weed (28G)', 28, 0, 1),
+	('coke', 'Coke (1G)', 1, 0, 1),
+	('coke_pooch', 'Bag of coke (28G)', 28, 0, 1),
 	('meth', 'Meth (1G)', 1, 0, 1),
-	('meth_pooch', 'Meth Tasche (28G)', 28, 0, 1),
+	('meth_pooch', 'Bag of meth (28G)', 28, 0, 1),
 	('opium', 'Opium (1G)', 1, 0, 1),
-	('opium_pooch', 'Opium Tasche (28G)', 28, 0, 1)
+	('opium_pooch', 'Bag of opium (28G)', 28, 0, 1)
 ;
